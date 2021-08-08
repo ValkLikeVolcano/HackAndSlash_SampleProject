8a6b6ec3480e8be92aaa05e7e863b0e59fc1b848
ValkLikeVolcano <nico@martin.li>
2021-08-08 18:39:11 +0200

    Completely Redone Movement
    Its now no longer physics Based but works using a Character controller. This is A much simpler and Better Technique for HS Games

--- HackAndSlashProject/Assets/Scripts/PlayerMovement.cs
+++ HackAndSlashProject/Assets/Scripts/PlayerMovement.cs
@@ -3,154 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-	private Rigidbody rb;
-    public Transform orientation;
-    public Transform cam;
+	CharacterController controller;
+	public Transform cam;
+	public Transform groundCheck;
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
 
-    //Rotation and look
-    private float xRotation;
-    private float sensitivity = 50f;
-    private float sensMultiplier = 1f;
+	public float speed = 5f;
+	public float jumpHeight = 4f;
+	public float turnSmoothTime = 0.1f;
+	float turnSmoothVelocity;
+	public float gravity = -9.81f;
 
-    //Movement
-    public float moveSpeed = 4500;
-    public float maxSpeed = 20;
-    public bool grounded;
-    public LayerMask whatIsGround;
-    public float groundOffset = 1f;
-    public float groundRadius = 0.2f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+	Vector3 velocity;
+	bool grounded;
 
-    public float counterMovement = 0.175f;
-    private float threshold = 0.01f;
-    public float maxSlopeAngle = 35f;
-
-    //Jumping
-    private bool readyToJump = true;
-    private float jumpCooldown = 0.25f;
-    public float jumpForce = 300f;
-
-    bool jumping;
-
-    float x, y;
-
-    Vector3 groundcheckPos;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        groundcheckPos = new Vector3(transform.position.x, transform.position.y - groundOffset, transform.position.z);
-    }
 	private void Start()
 	{
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+		controller = GetComponent<CharacterController>();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = true;
+	}
+
 	private void Update()
 	{
-        groundcheckPos = new Vector3(transform.position.x, transform.position.y - groundOffset, transform.position.z);
-        grounded = Physics.CheckSphere(groundcheckPos, groundRadius, whatIsGround);
+		grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
-        jumping = Input.GetButton("Jump");
-    }
-	private void FixedUpdate()
-	{
-        Vector2 mag = FindVelRelativeToLook();
-        float xMag = mag.x, yMag = mag.y;
-        Vector3 dir = new Vector3(x, 0f, y).normalized;
-
-        CounterMovement(x, y, mag);
-
-        if (readyToJump && jumping) Jump();
-
-        float maxSpeed = this.maxSpeed;
-
-        if (x > 0 && xMag > maxSpeed) x = 0;
-        if (x < 0 && xMag < -maxSpeed) x = 0;
-        if (y > 0 && yMag > maxSpeed) y = 0;
-        if (y < 0 && yMag < -maxSpeed) y = 0;
-
-        if(dir.magnitude >= 0.1f)
+		if(grounded && velocity.y < 0)
 		{
-            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        }
+			velocity.y = -2f;
+		}
 
-        rb.AddForce(transform.forward * y * moveSpeed * Time.deltaTime);
-        rb.AddForce(transform.right * x * moveSpeed * Time.deltaTime);
-    }
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
 
-    public Vector2 FindVelRelativeToLook()
-    {
-        float lookAngle = orientation.transform.eulerAngles.y;
-        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+		Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
 
-        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
-        float v = 90 - u;
+		if(dir.magnitude >= 0.1f)
+		{
+			float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+			transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        float magnitue = rb.velocity.magnitude;
-        float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
-        float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
+			Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+			controller.Move(moveDir.normalized * speed * Time.deltaTime);
+		}
 
-        return new Vector2(xMag, yMag);
-    }
+		if (Input.GetButtonDown("Jump") && grounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
 
-    private void CounterMovement(float x, float y, Vector2 mag)
-    {
-        if (!grounded || jumping) return;
+		velocity.y += gravity * Time.deltaTime;
 
-        //Counter movement
-        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
-        {
-            rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
-        }
-        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
-        {
-            rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
-        }
-
-        //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
-        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
-        {
-            float fallspeed = rb.velocity.y;
-            Vector3 n = rb.velocity.normalized * maxSpeed;
-            rb.velocity = new Vector3(n.x, fallspeed, n.z);
-        }
-    }
-
-    private void Jump()
-    {
-        if (grounded && readyToJump)
-        {
-            readyToJump = false;
-
-            //Add jump forces
-            rb.AddForce(Vector2.up * jumpForce * 1.5f);
-
-            //If jumping while falling, reset y velocity.
-            Vector3 vel = rb.velocity;
-            if (rb.velocity.y < 0.5f)
-                rb.velocity = new Vector3(vel.x, 0, vel.z);
-            else if (rb.velocity.y > 0)
-                rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-    }
-
-    private void ResetJump()
-    {
-        readyToJump = true;
-    }
-
-	private void OnDrawGizmosSelected()
-	{
-        Gizmos.DrawSphere(groundcheckPos, groundRadius);
+		controller.Move(velocity * Time.deltaTime);
 	}
 }
